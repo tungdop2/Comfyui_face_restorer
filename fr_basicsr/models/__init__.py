c712260686db987a7ccd6c5fcf9cf157b1ce21a5
tungdop2 <tungdop2@gmail.com>
2024-11-21 22:51:09 +0700

    fix init

--- fr_basicsr/models/__init__.py
+++ fr_basicsr/models/__init__.py
@@ -12,7 +12,7 @@ __all__ = ['build_model']
 model_folder = osp.dirname(osp.abspath(__file__))
 model_filenames = [osp.splitext(osp.basename(v))[0] for v in scandir(model_folder) if v.endswith('_model.py')]
 # import all the model modules
-_model_modules = [importlib.import_module(f'r_basicsr.models.{file_name}') for file_name in model_filenames]
+_model_modules = [importlib.import_module(f'fr_basicsr.models.{file_name}') for file_name in model_filenames]
 
 
 def build_model(opt):
